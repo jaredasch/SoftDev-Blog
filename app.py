ce483b45bcbbfeb3ba0bfcb0a5e488991e2faa71
Imad <ibelkebir@stuy.edu>
2018-10-22 21:14:05 -0400

    hashed passwords

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
-from util.db_utils import create_user, login_user, insert_post, get_posts, edit_post, get_post, get_all_posts
+from util.db_utils import count_users, create_user, login_user, get_user, insert_post, get_posts, edit_post
 from os import urandom
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_user = session.get("user"), posts = get_all_posts()[::-1])
+    return render_template("index.html", current_user = session.get("user"))
 
 
 @app.route("/signup", methods = ["GET", "POST"])
@@ -15,14 +16,16 @@ def signup():
     if "user" in session.keys():
         return redirect(url_for("index"))
     if request.method == "GET":
-        return render_template("signup.html", title = "Sign Up", current_user = session.get("user"))
+        return render_template("signup.html", title = "Sign Up")
     else:
         username = request.form.get("username")
-        password = request.form.get("password")
-        if create_user(username, password):
-            login_user(username, password)
+        password = request.form.get("password").encode('utf-8')
+        hash_obj = hashlib.md5(password)
+        hashpass = hash_obj.hexdigest()
+        if create_user(username, hashpass):
+            login_user(username, hashpass)
             return redirect(url_for("index"))
-        return render_template("signup.html", title = "Sign Up", current_user = session.get("user"))
+        return render_template("signup.html", title = "Sign Up")
 
 
 @app.route("/login", methods = ["GET", "POST"])
@@ -30,13 +33,15 @@ def login():
     if "user" in session.keys():
         return redirect(url_for("index"))
     if request.method == "GET":
-        return render_template("login.html", title = "Login", current_user = session.get("user"))
+        return render_template("login.html", title = "Login")
     else:
         username = request.form.get("username")
-        password = request.form.get("password")
-        login = login_user(username, password)
+        password = request.form.get("password").encode('utf-8')
+        hash_obj = hashlib.md5(password)
+        hashpass = hash_obj.hexdigest()
+        login = login_user(username, hashpass)
         if login == None:
-            return render_template("login.html", title = "Login", current_user = session.get("user"))
+            return render_template("login.html", title = "Login")
         return redirect(url_for("index"))
 
 
@@ -49,28 +54,24 @@ def logout():
 
 @app.route("/u/<username>", methods = ["GET"])
 def profile(username):
-    return render_template("profile.html", user = username, posts = get_posts(username)[::-1], current_user = session.get("user"))
+    return render_template("profile.html", user = username, posts = get_posts(username), current_user = session.get("user"))
 
 
-@app.route("/create_post", methods = ["POST"])
+@app.route("/create_post", methods = ["GET", "POST"])
 def create_post():
-    insert_post(request.form.get("blog_post"))
-    return redirect(request.referrer)
-
-
-@app.route("/edit/<int:post_id>", methods=["GET", "POST"])
-def edit(post_id):
     if request.method == "GET":
-        post = get_post(post_id)
-        if(post[1] != session.get("user")):
-            return redirect(url_for("profile", username = post[1]))
-        return render_template("profile.html", user = session.get("user"), current_user = session.get("user"), posts = get_posts(get_post(post_id)[1])[::-1], edit_id = post_id)
+        return render_template("make_post.html")
+    insert_post(request.form.get("blog_post"))
+    return redirect(url_for("index"))
 
-    post = get_post(post_id)
-    if(post[1] != session.get("user")):
-        return redirect(url_for("profile", username = post[1]))
-    edit_post(post_id, request.form.get("blog_post"))
-    return render_template("profile.html", user = post[1], posts = get_posts(get_post(post_id)[1])[::-1], current_user = session.get("user"))
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "GET":
+        return render_template("edit.html", blog_data = get_posts(session.get("user"))[0][2])
+    id = get_posts(session.get("user"))[0][0]
+    edit_post(request.form.get("blog_post"), id)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.debug = True
